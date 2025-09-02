e782419d794223bedbc56d0aba249e3960261a10
Edgar Trejo Avila <edgar.avila.rro@gmail.com>
2025-09-02 09:59:20 -0600

    Creé una interfaz súper mínima de la aplicación

--- lib/main_app.dart
+++ lib/main_app.dart
@@ -10,11 +10,90 @@ class MainApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: GridView.count(
-        crossAxisCount: 2,
-        children: List.generate(
-          3,
-          (index) => Card.filled(child: Text("Fuente de dinero $index")),
+      home: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [
+                      Colors.purple,
+                      Colors.cyanAccent,
+                      Colors.yellowAccent,
+                    ],
+                  ),
+                ),
+                padding: const EdgeInsets.all(8),
+                child: const Text("Hola <username>, bienvenido de vuelta"),
+              ),
+              const SizedBox(height: 16),
+              const Text(
+                "Fuentes de dinero",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 400,
+                child: GridView.count(
+                  scrollDirection: Axis.horizontal,
+                  crossAxisSpacing: 10,
+                  shrinkWrap: true,
+                  crossAxisCount: 2,
+                  children: List.generate(
+                    5,
+                    (index) => Container(
+                      width: 500,
+                      margin: const EdgeInsets.all(8),
+                      color: Colors.grey[200],
+                      child: const Placeholder(),
+                    ),
+                  ),
+                ),
+              ),
+              const Text(
+                "Categorías",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 400,
+                child: GridView.count(
+                  scrollDirection: Axis.horizontal,
+                  crossAxisSpacing: 10,
+                  shrinkWrap: true,
+                  crossAxisCount: 2,
+                  children: List.generate(
+                    5,
+                    (index) => Container(
+                      width: 500,
+                      margin: const EdgeInsets.all(8),
+                      color: Colors.grey[200],
+                      child: const Placeholder(),
+                    ),
+                  ),
+                ),
+              ),
+              const Text(
+                "Pagos",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 400,
+                child: ListView(
+                  children: List.generate(
+                    3,
+                    (_) => Container(
+                      height: 100,
+                      color: Colors.grey[200],
+                      child: Placeholder(),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
